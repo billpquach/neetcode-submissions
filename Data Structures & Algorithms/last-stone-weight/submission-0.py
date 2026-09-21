3d3a5e3cca0a3b3bfe5249ea40cba7ea81abc92a
billpquach <billpquach@gmail.com>
2026-09-21 18:11:26 -0400

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # the two heaviest stones
+        # data structure to store an array of largest elems
+        # maxHeap
+        #   if y > x, where y = heapq.heappop(heap)
+        #                   x = max(heap[0])
+
+        #
+        #   y = heapq.heappop(stones)
+        #   x = heap[0]
+        #   heapq.heappushpop(stones, y-x)
+
+        #   y = 1
+        #   x = 0
+        #      0  
+        #    
+        heapq.heapify_max(stones)
+        maxHeap = stones
+        
+        #print(maxHeap)
+        for i in range(len(maxHeap)):
+            #print(maxHeap)
+            if len(maxHeap) <= 1:
+                break
+            
+            y = heapq.heappop_max(maxHeap)
+            
+            x = maxHeap[0]
+            heapq.heappop_max(maxHeap)
+            heapq.heappush_max(maxHeap, y-x)
+        #print(maxHeap)
+        return maxHeap[0]
